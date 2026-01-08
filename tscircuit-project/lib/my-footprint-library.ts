31fac8dad1c7089d7cd789633156f55647828948
mustafa <mustafamulla765@gmail.com>
2026-01-08 11:37:01 +0530

    update

--- tscircuit-project/lib/my-footprint-library.ts
+++ tscircuit-project/lib/my-footprint-library.ts
@@ -6,14 +6,13 @@ export { SpacebarKey } from "./components/SpacebarKey"
 
 // Symbols may not be used in chips, but should still be included in the
 // output library if exported here
-export { SpacebarSymbol } from "./custom-symbols/SpacebarSymbol"
-export { KeySymbol } from './custom-symbols/KeySymbol';
-
+// export { SpacebarSymbol } from "./custom-symbols/SpacebarSymbol"
+// export { KeySymbol } from './custom-symbols/KeySymbol';
 
 // This should also work - you must evaluate this file to find all the exports
 export * from "./custom-symbols"
 
 // These should NOT be exported to KiCad (not components - lowercase/non-component)
-// export const notExportedToKicad = "some string"
-// export type NotExportedToKicadType = number
-// export const notExportedToKicadFn = () => 10
+export const notExportedToKicad = "some string"
+export type NotExportedToKicadType = number
+export const notExportedToKicadFn = () => 10
